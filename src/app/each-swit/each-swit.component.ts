75db63e3034012ba21907fe14e8da8c27d69371f
Sigit Prayoga <sigit.prayoga@obortech.com>
2016-11-01 00:18:21 +0700

    Add momentjs

--- src/app/each-swit/each-swit.component.ts
+++ src/app/each-swit/each-swit.component.ts
@@ -1,25 +1,38 @@
+import * as moment from 'moment';
+
 import { Component, OnInit, Input } from '@angular/core';
 import { Swit } from '../swit';
 import { ApiService } from '../api.service';
 
+
 @Component({
-  selector: 'each-swit',
-  templateUrl: './each-swit.component.html',
-  styleUrls: ['./each-swit.component.css']
+	selector: 'each-swit',
+	templateUrl: './each-swit.component.html',
+	styleUrls: ['./each-swit.component.css']
 })
 export class EachSwitComponent implements OnInit {
 
 	@Input()
 	swit: Swit;
 
-  	constructor(private apiService: ApiService) { }
+	constructor(private apiService: ApiService) {
+		
+	}
 
-  	ngOnInit() {
+	ngOnInit() {
+	}
 
-  	}
+	getMoment(date: Date) {
+		//convert to be like '3 minutes ago'
+		var date = new Date(date);
+		//get the hours from current swit
+		var hour = date.getHours();
+		//convert and return
+		return moment(date).fromNow();
+	}
 
-	like(switObj){
-		if(switObj && switObj.switId){
+	like(switObj) {
+		if (switObj && switObj.switId) {
 			this.apiService.likeSwit(switObj.switId, 'userId').subscribe(
 				res => {
 					//should change this 'userId' with the real id of the current user
